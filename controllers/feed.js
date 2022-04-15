a1a11777c91b4ee24bb1bbbcce2b085a0fad7572
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-04-15 16:00:48 +0200

    REST API: Fetch Posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -2,8 +2,14 @@ exports.getPosts = (_req, res, _next) => {
   res.status(200).json({
     posts: [
       {
+        _id: '1',
         title: 'First Post',
-        content: 'This is the first post!'
+        content: 'This is the first post!',
+        imageUrl: 'images/duck.jpg',
+        creator: {
+          name: 'Max'
+        },
+        createdAt: new Date()
       }
     ]
   });
