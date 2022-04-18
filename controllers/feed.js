2cdf615fbfadf1011167a6784afb7e21f8bfcd22
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-04-18 13:50:01 +0200

    REST API: Store Posts in DB

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 
+const Post = require('../models/post');
+
 exports.getPosts = (_req, res, _next) => {
   res.status(200).json({
     posts: [
@@ -24,16 +26,21 @@ exports.createPost = (req, res, _next) => {
   }
   const title = req.body.title;
   const content = req.body.content;
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: {
-      _id: new Date().toISOString(),
-      title,
-      content,
-      creator: {
-        name: 'Max'
-      },
-      createdAt: new Date()
-    }
+  const post = new Post({
+    title,
+    content,
+    imageUrl: 'images/duck.jpg',
+    creator: {
+      name: 'Paul'
+    },
   });
+  post.save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: result
+      });
+    })
+    .catch(err => console.log(err));
 };
