615d903076269772750022a5991f3e65c769141c
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-05-26 21:31:57 +0200

    GraphQL: Add Login

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use('/graphql', graphqlHTTP({
   schema: graphqlSchema,
   rootValue: graphqlResolver,
   graphiql: true,
-  formatError(err) {
+  customFormatErrorFn(err) {
     if (!err.originalError) {
       return err;
     }
