78c287e518915f78e2f4836dd0ca28680c5f7617
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-05-27 12:57:24 +0200

    GraphQL: Create and Get Post

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,24 @@
 const jwt = require('jsonwebtoken');
 
-const { throwError } = require('../util/error-handler');
-
 module.exports = (req, _res, next) => {
   const authHeader = req.get('Authorization');
   if (!authHeader) {
-    throwError('Not Authenticated.', 401);
+    req.isAuth = false;
+    return next();
   }
   const token = authHeader.split(' ')[1];
   let decodedToken;
   try {
     decodedToken = jwt.verify(token, 'somesupersecretsecret')
   } catch (err) {
-    err.statusCode = 500;
-    throw err;
+    req.isAuth = false;
+    return next();
   }
   if (!decodedToken) {
-    throwError('Not authenticated.', 401);
+    req.isAuth = false;
+    return next();
   }
   req.userId = decodedToken.userId;
+  req.isAuth = true;
   next();
 }
